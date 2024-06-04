03f4046b94e6c27c706e3dd22bb89a6891074d6f
Shawn <shawn.for.office@gmail.com>
2024-06-04 22:56:31 +0800

    replace the term 'rank' into 'level'

--- analysis_pipeline/integrate_analysis.py
+++ analysis_pipeline/integrate_analysis.py
@@ -21,39 +21,39 @@ class IntegrateAnalysis(IntegrateSamples):
     def get_hap_size(self, sample_id, hap):
         return int(self.sample_data[sample_id].hap_size[hap])
 
-    def get_rankname2abundance_dict(self, sample_id, rank):
+    def get_levelname2abundance_dict(self, sample_id, level):
         abundance = {}
-        for hap, rank_dict in self.sample_data[sample_id].hap2rank.items():
-            rank_name = rank_dict[rank]
-            if rank_name not in abundance:
-                abundance[rank_name] = 0
+        for hap, level_dict in self.sample_data[sample_id].hap2level.items():
+            level_name = level_dict[level]
+            if level_name not in abundance:
+                abundance[level_name] = 0
             size = self.get_hap_size(sample_id, hap)
-            abundance[rank_name] += size
+            abundance[level_name] += size
         return abundance # e.g. {'SpA': 3, 'SpB': 4, 'SpC': 5}
 
-    def barchart_relative_abundance(self, rank, save_html_dir=None, save_html_name=None, sample_id_list=None):
-        print(f"> Plotting barchart for {rank}...")
+    def barchart_relative_abundance(self, level, save_html_dir=None, save_html_name=None, sample_id_list=None):
+        print(f"> Plotting barchart for {level}...")
         sample_id_list = self.get_sample_id_list(sample_id_list)
 
         samples_abundance = {}
         for sample_id in sample_id_list:
-            abundance = self.get_rankname2abundance_dict(sample_id, rank)
+            abundance = self.get_levelname2abundance_dict(sample_id, level)
             samples_abundance[sample_id] = normalize_abundance(abundance)
 
-        all_rank_name = [list(samples_abundance[sample_id].keys()) for sample_id in sample_id_list]
-        uniq_rank_name = list_union(all_rank_name)
+        all_level_name = [list(samples_abundance[sample_id].keys()) for sample_id in sample_id_list]
+        uniq_level_name = list_union(all_level_name)
     
         for sample_id in sample_id_list:
-            samples_abundance[sample_id] = [samples_abundance[sample_id].get(rank_name, 0) for rank_name in uniq_rank_name]
+            samples_abundance[sample_id] = [samples_abundance[sample_id].get(level_name, 0) for level_name in uniq_level_name]
  
-        plotdata = pd.DataFrame(samples_abundance, index=uniq_rank_name)
+        plotdata = pd.DataFrame(samples_abundance, index=uniq_level_name)
         fig = create_barchart_fig(plotdata.transpose())
         fig.show()
         print("> Barchart generated.")
 
         if save_html_dir != None:
             if save_name == None:
-                save_name = f"{rank}_bar_chart"
+                save_name = f"{level}_bar_chart"
             bar_chart_path = os.path.join(save_html_dir, f'{save_html_name}.html')
             fig.write_html(bar_chart_path)
             print(f"> Barchart saved to:  {bar_chart_path}")
@@ -61,10 +61,10 @@ class IntegrateAnalysis(IntegrateSamples):
     def get_units2fasta_dict(self, target_name, target_level, units_level, sample_id_list):
         units2fasta = {}
         for sample_id in sample_id_list:
-            for hap, rank_dict in self.sample_data[sample_id].hap2rank.items():
-                if target_name not in rank_dict[target_level]:
+            for hap, level_dict in self.sample_data[sample_id].hap2level.items():
+                if target_name not in level_dict[target_level]:
                     continue
-                unit_name = rank_dict[units_level]
+                unit_name = level_dict[units_level]
                 title = f"{unit_name}-{sample_id}_{hap}"
                 seq = self.sample_data[sample_id].hap_seq[hap]
 
@@ -170,10 +170,10 @@ if __name__ == '__main__':
     # a.import_samples(read_dir=read_dir)
     # a.save_sample_data(save_dir=read_dir, save_name='all_site_alpha_8')
     a = IntegrateAnalysis(load_path=load_path)
-    # a.umap_target(target_name='Chordata', target_rank='phylum', units_rank='family', neighbors=15, min_dist=0.5, save_dir='.\\test_umap',)
+    # a.umap_target(target_name='Chordata', target_level='phylum', units_level='family', neighbors=15, min_dist=0.5, save_dir='.\\test_umap',)
     # print(a.sample_id_list)
-    # a.barchart_relative_abundance(rank='species')
+    # a.barchart_relative_abundance(level='species')
     # a.umap_target(target_list=["Blenniidae", "Labridae", "Mugilidae", "Pomacentridae", "Tripterygiidae"], target_level='family', units_level='species', neighbors=15, min_dist=0.5, parent_dir='.\\..\\..\\test_umap', plot_all=True, plot_sep=True,)
     a.cluster_umap(index_file='.\\..\\..\\test_umap\\umap_result\\index.tsv', n_unit_threshold=15, min_samples=5, min_cluster_size=5, plot_all=True, plot_sep=True)
-    # a.mltree_target(target_name='Mugilidae', target_rank='family', dereplicate=True, model=None, bootstrap=999, threads=None, save_dir='.\\test_mltree')
-    # a.umap_target(target_name='Mugilidae', target_rank='family', units_rank='species', neighbors=15, min_dist=0.5, save_dir='.\\test_umap')
+    # a.mltree_target(target_name='Mugilidae', target_level='family', dereplicate=True, model=None, bootstrap=999, threads=None, save_dir='.\\test_mltree')
+    # a.umap_target(target_name='Mugilidae', target_level='family', units_level='species', neighbors=15, min_dist=0.5, save_dir='.\\test_umap')
