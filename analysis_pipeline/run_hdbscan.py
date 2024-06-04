42679e710c81c5670a7afa021a3fb98008d2e69d
Shawn <shawn.for.office@gmail.com>
2024-06-04 13:45:27 +0800

    minor changing for setting color style by 'cmap' variable'

--- analysis_pipeline/run_hdbscan.py
+++ analysis_pipeline/run_hdbscan.py
@@ -9,21 +9,19 @@ def fit_hdbscan(points, min_samples=5, min_cluster_size=5):
         min_cluster_size=min_cluster_size
         ).fit_predict(points)
     clustered = (labels >= 0)
-
+    num_c = max(labels) + 1
+    noise = sum(1 for i in labels if i < 0)
+    print(f'Number of clusters: {num_c}')
+    print(f'clustered percentage: {100 - noise / len(labels) * 100:.2f}%')
     return labels, clustered
 
-def plot_hdbscan(points, labels, clustered, png_path, theme=None, cmap="Spectral", background="white", width=800, height=800):
+def plot_hdbscan(points, labels, clustered, png_path, cmap="Spectral", background="white", width=800, height=800):
     dpi = plt.rcParams["figure.dpi"]
     fig = plt.figure(figsize=(width / dpi, height / dpi))
     ax = fig.add_subplot(111)
     ax.set_facecolor(background)
     
     point_size = 300.0 / np.sqrt(points.shape[0])
-
-    if theme is not None:
-        cmap = _themes[theme]["cmap"]
-        if background is None:
-            background = _themes[theme]["background"]
 
     ax.scatter(
         points[~clustered, 0],
