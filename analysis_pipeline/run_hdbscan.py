8814f0e92aea772ce9beec570ec11774be10efc1
Shawn <shawn.for.office@gmail.com>
2024-06-05 10:55:35 +0800

    add hdbscan parameters & removed the 'remove_row_by_unit_occurance' function

--- analysis_pipeline/run_hdbscan.py
+++ analysis_pipeline/run_hdbscan.py
@@ -1,17 +1,18 @@
 import hdbscan
 import matplotlib.pyplot as plt
 import numpy as np
-from run_umap import remove_row_by_unit_occurance
 
 def fit_hdbscan(points, min_samples=5, min_cluster_size=5):
     labels = hdbscan.HDBSCAN(
         min_samples=min_samples,
-        min_cluster_size=min_cluster_size
+        min_cluster_size=min_cluster_size,
+        cluster_selection_epsilon=0.5,
+        alpha=1.0,
+        allow_single_cluster=True
         ).fit_predict(points)
     clustered = (labels >= 0)
     n_c = max(labels) + 1
     noise = sum(1 for i in labels if i < 0)
-    # keep 2 after .
     c_p = (1 - noise / len(labels)) * 100
     return labels, clustered, n_c, round(c_p, 2)
 
@@ -38,12 +39,11 @@ def plot_hdbscan(points, labels, clustered, png_path, cmap="Spectral", backgroun
         cmap=cmap)
     ax.figure.savefig(png_path)
 
-def run_hdbscan(index, n_unit_threshold=1, min_samples=5, min_cluster_size=5, png_path="./clustered.png", cmap="Spectral"):
+def run_hdbscan(index, min_samples=5, min_cluster_size=5, png_path="./clustered.png", cmap="Spectral"):
     print(f'\n> Clustering...')
-    removed_index = remove_row_by_unit_occurance(index, n_unit_threshold)
-    points = removed_index[["umap1", "umap2"]].to_numpy()
+    points = index[["umap1", "umap2"]].to_numpy()
     labels, clustered, numb_clus, clus_perc = fit_hdbscan(points, min_samples, min_cluster_size)
-    numb_unit = len(removed_index["unit"].unique())
+    numb_unit = len(index["unit"].unique())
     plot_hdbscan(points, labels, clustered, png_path, cmap)
     print(f'Saved PNG to: {png_path}')
     return numb_unit, numb_clus, clus_perc
