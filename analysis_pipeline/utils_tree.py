161aaea658812af45e86571cca24410a4bcf1fe1
Shawn <shawn.for.office@gmail.com>
2024-07-07 16:20:09 +0800

    separate tree function from analysis_function module to utils_tree module

---
+++ analysis_pipeline/utils_tree.py
@@ -0,0 +1,68 @@
+import os
+import subprocess
+
+def check_mltree_overwrite(save_dir: str, prefix: str) -> str:
+    # TODO(SW): Eventually, replace with argparse.
+    """
+    Check if an MLTree run with the given prefix already exists in the specified directory. 
+    If it does, prompt the user to decide whether to redo the run or not.
+
+    :param save_dir: Directory where the output files are saved.
+    :param prefix: Prefix for the output file names.
+    :return: User input choice ('-redo', '--redo-tree', '--undo', or 'stop').
+    """
+    ckp_path = os.path.join(save_dir, prefix + '.ckp.gz') # e.g. save/dir/SpA.ckp.gz
+    if os.path.exists(ckp_path):
+        print(
+            f"> MLTree checkpoint fileCheckpoint ({ckp_path}) indicates that a previous run successfully finished  already exists.\n"
+            "Use `-redo` option if you really want to redo the analysis and overwrite all output files.\n"
+            "Use `--redo-tree` option if you want to restore ModelFinder and only redo tree search.\n"
+            "Use `--undo` option if you want to continue previous run when changing/adding options.\n"
+        )
+        user_input_choice = ['-redo', '--redo-tree', '--undo', 'stop']
+        help_msg = f"({'/'.join(user_input_choice)}): "
+        while True:
+            user_input = input(help_msg).strip().lower()
+            if user_input in user_input_choice:
+                return user_input
+            print("> Invalid input.")
+    else:
+        return ''
+
+def iqtree2_command(seq_path:str, save_dir: str, prefix: str, model:str = None, bootstrap: int = None, threads: int = None) -> None:
+    """
+    Run IQTREE2 command with specified options.
+
+    :param seq_path: Path to the input aligned sequence file.
+    :param save_dir: Directory to save the output files.
+    :param prefix: Prefix for the output file names.
+    :param model: Model to specify for tree inference. If not specified, it will use the best-fit model found (default: None).
+    :param bootstrap: Number of bootstrap replicates (default: None).
+    :param threads: Number of threads to use. If not specified, it will automatically determine the best number of cores given the current data and computer (default: None).
+    """
+    os.makedirs(save_dir, exist_ok=True)
+
+    checkpoint = check_mltree_overwrite(save_dir, prefix)
+    if checkpoint == 'stop':
+        print("> Stopping the run.")
+        return
+
+    model = model or 'TEST'
+    prefix_path = os.path.join(save_dir, prefix)
+    bootstrap_string = f'-b {bootstrap} ' if bootstrap else ''
+    threads = threads or 'AUTO'
+
+    cmd = [
+        'iqtree2', '-m', model, '-s', seq_path, '--prefix', prefix_path,
+        bootstrap_string, '-nt', threads, checkpoint
+    ]
+    cmd = [c for c in cmd if c]
+
+    print("> Running IQTREE2 command: ", ' '.join(cmd))
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"> IQTREE2 finished. Output files are in {save_dir}.")
+    except subprocess.CalledProcessError as e:
+        print(f"> Error occurred during IQTREE2 run: {e}")
+
+iqtree2_command(seq_path='.\\..\\..\\test.fa', save_dir='.\\..\\..\\test_dir', prefix='test')
