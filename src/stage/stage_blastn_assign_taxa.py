c31291191d9b5604db1ffddab7f9a932e277beb2
Shawn <shawn.for.office@gmail.com>
2024-07-22 15:16:20 +0800

    modified the function to add taxonomic level

--- src/stage/stage_blastn_assign_taxa.py
+++ src/stage/stage_blastn_assign_taxa.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import os
 import pandas as pd
@@ -6,8 +7,28 @@ from stage.stage_builder import StageBuilder
 
 
 class AssignTaxaStage(StageBuilder):
+
+    @staticmethod
+    def parse_genus2otherlv(lineage_path: str) -> dict[str, list[str]]:
+        genus2otherlv = {}
+        with open(lineage_path) as in_handle:
+            reader = csv.DictReader(in_handle)
+            for row in reader:
+                genus_name = [row['genus_name']]
+                otherlv = [
+                    row['family_name'],
+                    row['order_name'],
+                    row['class_name'],
+                    row['phylum_name'],
+                    row['kingdom_name'],
+                ]
+                genus2otherlv[genus_name] = otherlv
+
+        return genus2otherlv
+
     def __init__(self, config, heading="assign_taxa", denoise_dir="", save_dir="",
                  db_path: str = "",
+                 lineage_path: str = "",
                  maxhitnum: int = 1,
                  evalue: float = 0.00001,
                  qcov_hsp_perc: int = 90,
@@ -19,6 +40,9 @@ class AssignTaxaStage(StageBuilder):
         self.BLAST_PROG = "blastn"
         self.denoise_dir = denoise_dir
         self.save_dir = save_dir
+        self.blast_outfile = None
+        self.lineage_path = lineage_path
+        self.genus2otherlv = self.parse_genus2otherlv(lineage_path)
         self.parse_params(db_path, maxhitnum, evalue, qcov_hsp_perc, perc_identity, outfmt, specifiers)
 
     def parse_params(self, db_path, maxhitnum, evalue, qcov_hsp_perc, perc_identity, outfmt, specifiers):
@@ -32,49 +56,44 @@ class AssignTaxaStage(StageBuilder):
         )
 
     def setup(self, prefix):
-        self.prefix = prefix
         infile = os.path.join(self.denoise_dir, f"{prefix}_denoise.fasta")
-        blast_outfile = os.path.join(self.save_dir, f"{prefix}_blast.csv")
+        self.blast_outfile = os.path.join(self.save_dir, f"{prefix}_blast.csv")
         cmd = (
             f"{self.BLAST_PROG} -query {infile}"
             f" {self.params}"
             f" -num_threads {self.config.n_cpu}"
-            f" -out {blast_outfile}"
+            f" -out {self.blast_outfile}"
         )
         super().add_stage("ncbi-blast+_blastn", cmd)
 
-        return blast_outfile
-
-    @staticmethod
-    def _add_taxonomy(outfile, genus2taxonomy):
-        blast_result = pd.read_csv(outfile, header=None)
+    def add_taxonomy(self):
+        blast_result = pd.read_csv(self.blast_outfile, header=None)
         taxa_matrix = []
         for sseqid in blast_result[1]:
-            sacc, species = sseqid.split('|')[1], sseqid.split('|')[-1]
-            species_firstname = species.split('_')[0]
-            if species_firstname in genus2taxonomy.keys():
-                genus = species_firstname
+            species = sseqid.split('|')[-1]
+            genus = species.split('_')[0]
+            if genus in self.genus2otherlv.keys():
+                taxonomy_levels = [species, genus] + self.genus2otherlv[genus]
             else:
-                genus = [genus_level for genus_level, other_levals in genus2taxonomy.items() if species_firstname in other_levals][0]
-            taxa_matrix.append([sacc, species, genus, genus2taxonomy[genus][4], genus2taxonomy[genus][3], genus2taxonomy[genus][2], genus2taxonomy[genus][1], genus2taxonomy[genus][0]])
-        taxa_matrix = np.array(taxa_matrix)
-        sacc_list, species_list, genus_list, family_list, order_list, class_list, phylum_list, kingdom_list = taxa_matrix[:, 0].tolist(), taxa_matrix[:, 1].tolist(), taxa_matrix[:, 2].tolist(), taxa_matrix[:, 3].tolist(), taxa_matrix[:, 4].tolist(), taxa_matrix[:, 5].tolist(), taxa_matrix[:, 6].tolist(), taxa_matrix[:, 7].tolist()
-        blast_result[1] = sacc_list
-        blast_result.insert(2, '2', species_list)
-        blast_result.insert(3, '3', genus_list)
-        blast_result.insert(4, '4', family_list)
-        blast_result.insert(5, '5', order_list)
-        blast_result.insert(6, '6', class_list)
-        blast_result.insert(7, '7', phylum_list)
-        blast_result.insert(8, '8', kingdom_list)
-        blast_result.to_csv(outfile, index=False, header=None)
+                print(f"genus {genus} not found in: {self.lineage_path}")
+                taxonomy_levels =[species, genus] + [''] * 5
+            taxa_matrix.append(taxonomy_levels)
+
+        taxa_matrix_trans = np.array(taxa_matrix).T
+        for i, lv in enumerate(taxa_matrix_trans):
+            blast_result.insert(i+2, i+2, lv)
+
+        blast_result.to_csv(self.blast_outfile, index=False, header=None)
+
+        return True
 
     def run(self):
         super().run()
+        self.output.append(self.add_taxonomy())
         return all(self.output)
 
 
-def fq_to_fa_demo(config, prefix, denoise_dir="", save_dir=""):
+def assign_taxa_demo(config, prefix, denoise_dir="", save_dir=""):
     stage = AssignTaxaStage(config, denoise_dir=denoise_dir, save_dir=save_dir)
     outfile = stage.setup(prefix)
     is_complete = stage.run()
